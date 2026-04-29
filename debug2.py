96565aeed802157c8bc60ee460f58c23ff94098e
DataExplorer <bt23csd046@iiitn.ac.in>
2026-04-29 06:25:31 +0530

    Delete debug2.py

--- debug2.py
+++
@@ -1,8 +0,0 @@
-from src.triage import detect_red_flags, extract_symptoms
-
-text = "Baby has minor cuts from playing. No bleeding, alert and playing normally."
-flags = detect_red_flags(text)
-symptoms = extract_symptoms(text)
-print(f'Text: {text}')
-print(f'Detected red flags: {flags}')
-print(f'Detected symptoms: {symptoms}')
